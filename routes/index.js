7271cbd007e06be7603e351e309330dbc3f62567
Manuel Lázaro <mlazaroaat@gmail.com>
2015-07-23 21:40:54 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz p2p mlazaro' });
 });
+
+router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/question', quizController.question);
 
 module.exports = router;
